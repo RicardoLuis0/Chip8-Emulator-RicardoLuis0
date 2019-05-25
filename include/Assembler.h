3f9c89741332b2f42cad98feb8948b58107fe7aa
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-25 12:50:12 -0300

    assembler WIP1

--- include/Assembler.h
+++ include/Assembler.h
@@ -1,16 +1,11 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
+#include <vector>
+#include <string>
+#include "AssemblerToken.h"
 
-class Assembler
-{
-    public:
-        Assembler();
-        virtual ~Assembler();
-
-    protected:
-
-    private:
-};
-
+bool assemble_file(std::string input_filename,std::string output_filename);//read input file, tokenize, parse and assemble, write instructions into output, return success
+std::vector<uint16_t> assemble_tokens(std::vector<std::vector<AssemblerToken>>);//read tokenized asm source, return instruction list
+uint16_t assemble_instruction(std::vector<AssemblerToken> line);//read tokenized asm line, return instruction
 #endif // ASSEMBLER_H
--- src/Assembler/Assembler.cpp
+++ src/Assembler/Assembler.cpp
@@ -1,11 +1,13 @@
 #include "Assembler.h"
 
-Assembler::Assembler()
-{
-    //ctor
-}
+#include <stdexcept>
 
-Assembler::~Assembler()
-{
-    //dtor
+bool assemble_file(std::string input_filename,std::string output_filename){
+    throw std::runtime_error("Unimplemented");
+}
+std::vector<uint16_t> assemble_tokens(std::vector<std::vector<AssemblerToken>> tokens){
+    throw std::runtime_error("Unimplemented");
+}
+uint16_t assemble_instruction(std::vector<AssemblerToken> line){
+    throw std::runtime_error("Unimplemented");
 }
