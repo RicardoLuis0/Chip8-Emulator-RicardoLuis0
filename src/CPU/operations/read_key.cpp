aa1ed3d5a01b1a1cdc948f73864620a9896d93c1
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 07:31:50 -0300

    improve exception message

--- src/CPU/operations/read_key.cpp
+++ src/CPU/operations/read_key.cpp
@@ -2,5 +2,5 @@
 #include <stdexcept>
 
 void CPU::read_key(uint8_t X){//wait for keypress, and store it at V[X]
-    throw std::runtime_error("Unimplemented Operation");
+    throw std::runtime_error("Unimplemented Operation: Wait for keypress");
 }
