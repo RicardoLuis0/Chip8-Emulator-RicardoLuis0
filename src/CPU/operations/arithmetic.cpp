6065f73c0963a841a1e2dc2e54fb942a36431f03
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 06:54:54 -0300

    arithmetic operations

--- src/CPU/operations/arithmetic.cpp
+++ src/CPU/operations/arithmetic.cpp
@@ -2,21 +2,25 @@
 #include <stdexcept>
 
 void CPU::add(uint8_t X,uint8_t value){//add value to V[X]
-    throw std::runtime_error("Unimplemented Operation");
+    V[X]+=value;
 }
 
 void CPU::add_reg(uint8_t X,uint8_t Y){//add V[Y] to V[X]
-    throw std::runtime_error("Unimplemented Operation");
+    VF=(V[X]+V[Y]>0xFF);
+    V[X]+=V[Y];
 }
 
 void CPU::add_i(uint8_t X){//add V[X] to I
-    throw std::runtime_error("Unimplemented Operation");
+    VF=(I+V[X]>0xFFF);
+    I+=V[X];
 }
 
 void CPU::subtract(uint8_t X,uint8_t Y){//subtract V[Y] from V[X]
-    throw std::runtime_error("Unimplemented Operation");
+    VF=(V[X]-V[Y]>0);
+    V[X]-=V[Y];
 }
 
 void CPU::reverse_subtract(uint8_t X,uint8_t Y){//move V[Y]-V[X] to V[X]
-    throw std::runtime_error("Unimplemented Operation");
+    VF=(V[Y]-V[X]>0);
+    V[X]=V[Y]-V[X];
 }
