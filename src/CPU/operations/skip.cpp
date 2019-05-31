bf40ae1a7d92c5dd9be113ec3188626e89fa98f8
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 07:09:38 -0300

    skip operations

--- src/CPU/operations/skip.cpp
+++ src/CPU/operations/skip.cpp
@@ -2,25 +2,25 @@
 #include <stdexcept>
 
 void CPU::skip_if_eq(uint8_t X,uint8_t value){//skip next instruction if V[X] == value
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]==value)PC+=2;
 }
 
 void CPU::skip_if_eq_reg(uint8_t X,uint8_t Y){//skip next instruction if V[X] == V[Y]
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]==V[Y])PC+=2;
 }
 
 void CPU::skip_if_not_eq(uint8_t X,uint8_t value){//skip next instruction if V[X] != value
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]!=value)PC+=2;
 }
 
 void CPU::skip_if_not_eq_reg(uint8_t X,uint8_t Y){//skip next instruction if V[X] != V[Y]
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]!=V[Y])PC+=2;
 }
 
 void CPU::skip_key(uint8_t X){//skip next instruction if the key at V[X] is currently pressed
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]<16&&KB[V[X]])PC+=2;
 }
 
 void CPU::skip_not_key(uint8_t X){//skip next instruction if the key at V[X] is currently NOT pressed
-    throw std::runtime_error("Unimplemented Operation");
+    if(V[X]<16&&!KB[V[X]])PC+=2;
 }
