c40c250fb598e2c6612a2b66f119fb5a84df9d5c
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 08:22:06 -0300

    delay and sound counter updates

--- src/CPU/doCycle.cpp
+++ src/CPU/doCycle.cpp
@@ -1,7 +1,13 @@
 #include "CPU.h"
+#include <iostream>
 
 void CPU::doCycle(){
     if(waiting_for_input)return;
     RawInstruction d=nextInstruction();
     runInstruction(decodeInstruction(d),d);
+    if(DT>0)DT--;
+    if(ST>0){
+        std::cout<<"\a";//TODO replace with sdl sound stuff
+        ST--;
+    }
 }
