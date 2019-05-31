9d34369dc524ef27acaada56fc999cd14f01ebd2
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 07:25:09 -0300

    draw,clear operations

--- src/CPU/operations/draw_clear.cpp
+++ src/CPU/operations/draw_clear.cpp
@@ -1,10 +1,26 @@
 #include "CPU.h"
 #include <stdexcept>
+#include <cstring>
+
+#define VRAMPOS(x,y) (y*64+x)
+
+#define GETBIT(v,b) ((v<<b)&0x1)
 
 void CPU::clear_screen(){//clear screen
-    throw std::runtime_error("Unimplemented Operation");
+    memset(VRAM,0,sizeof(VRAM));
 }
 
 void CPU::draw_sprite(uint8_t X,uint8_t Y,uint8_t height){//draw sprite stored in memory location I at (V[X],V[Y]) coords, with given height
-    throw std::runtime_error("Unimplemented Operation");
+    VF=0x0;
+    for(int i=0;i<height;i++){
+        for(int j=0;j<8;j++){
+            if(GETBIT(RAM[I+i],j)){
+                size_t pos=VRAMPOS(X+j,Y+i);
+                VRAM[pos]^=1;
+                if(!VRAM[pos]){
+                    VF=0x1;
+                }
+            }
+        }
+    }
 }
