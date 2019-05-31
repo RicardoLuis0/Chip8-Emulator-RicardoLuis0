76f2203f3f241859659c3e778b19838da6e712d0
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 06:37:13 -0300

    add spacing between methods

--- src/CPU/operations/bitwise_operations.cpp
+++ src/CPU/operations/bitwise_operations.cpp
@@ -5,15 +5,19 @@
 void CPU::or_operation(uint8_t X,uint8_t Y){//move V[X] | V[Y] to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::and_operation(uint8_t X,uint8_t Y){//move V[X] & V[Y] to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::xor_operation(uint8_t X,uint8_t Y){//move V[X] ^ V[Y] to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::shift_right(uint8_t X,uint8_t Y){//shift V[X] to the right by 1, store least significant bit in V[Y]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::shift_left(uint8_t X,uint8_t Y){//shift V[X] to the left by 1, store most significant bit in V[Y]
     throw std::runtime_error("Unimplemented Operation");
 }
--- src/CPU/operations/move.cpp
+++ src/CPU/operations/move.cpp
@@ -4,18 +4,23 @@
 void CPU::move(uint8_t X,uint8_t value){//move value to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::move_reg(uint8_t X,uint8_t Y){//move V[Y] to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::move_i(uint8_t value){//move value to I
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::move_reg_dt(uint8_t X){//move DT to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::move_dt(uint8_t X){//move DT to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
+
 void CPU::move_st(uint8_t X){//move ST to V[X]
     throw std::runtime_error("Unimplemented Operation");
 }
