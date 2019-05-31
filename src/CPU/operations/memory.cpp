0a25e695480867ec174b7642fe7cce95e2fef929
RicardoLuis0 <ricolvs123@gmail.com>
2019-05-31 07:33:31 -0300

    memory operations

--- src/CPU/operations/memory.cpp
+++ src/CPU/operations/memory.cpp
@@ -2,17 +2,26 @@
 #include <stdexcept>
 
 void CPU::load_font(uint8_t X){//move the location of the font for V[X] to I
-    throw std::runtime_error("Unimplemented Operation");
+    I=font_addr[V[X]];
 }
 
 void CPU::binary_coded_decimal(uint8_t X){//store the binary coded decimal value of V[X] at I,I+1,I+2
-    throw std::runtime_error("Unimplemented Operation");
+    uint8_t t1=V[X]%10;
+    uint8_t t2=(V[X]/10)%10;
+    uint8_t t3=(V[X]/100)%10;
+    RAM[I]=t1;
+    RAM[I+1]=t2;
+    RAM[I+2]=t3;
 }
 
 void CPU::store(uint8_t X){//store the values of V[0]~V[X] to memory starting at I
-    throw std::runtime_error("Unimplemented Operation");
+    for(int i=0;i<=X;i++){
+        RAM[I+i]=V[i];
+    }
 }
 
 void CPU::load(uint8_t X){//load the values of V[0]~V[X] from memory starting at I
-    throw std::runtime_error("Unimplemented Operation");
+    for(int i=0;i<=X;i++){
+        V[i]=RAM[I+i];
+    }
 }
