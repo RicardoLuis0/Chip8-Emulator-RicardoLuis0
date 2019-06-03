cd2d7da5751294966ece4684cf9effc5634c1958
Ricardo <ricolvs123@gmail.com>
2019-06-03 20:39:49 -0300

    convert to curses WIP2

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "splitString.h"
 #include "Disassembler.h"
 #include "SDLHandler.h"
+#include "Debugger.h"
 
 #include <curses.h>
 #include "QuitException.h"
@@ -52,66 +53,67 @@ int main(int argc,char ** argv){
         move(sv2_y,sv2_x);
         refresh();
     }
-    std::cout<<"Seeding random number generator...";
+    printw("Seeding random number generator...");
     srand(time(NULL));
-    std::cout<<"Done.\nInitializing SDL2...";
+    printw("Done.\nInitializing SDL2...");
     if(SDL_Init(SDL_INIT_VIDEO)!=0){
-        std::cout<<"Error.\n >Error while Initializing SDL: "<<SDL_GetError()<<"\n";
+        printw("Error.\n >Error while Initializing SDL: %s\n",SDL_GetError());
         endwin();
         return 0;
     }
-    std::cout<<"Done.\n";
+    printw("Done.\n");
     try{
-    if(args.hasOption("disassemble")){
-        std::cout<<"Opening Input File...";
-        Program p=FileLoader::load(file,4096-0x200);
-        std::vector<disassembled_instruction> asm_vec;
-        std::cout<<"Done.\nStarting Disassembly...";
-        for(unsigned i=0;i<p.length;){
-            uint8_t byte0=p.data[i++];
-            if(i<p.length){
-                uint8_t byte1=p.data[i++];
-                asm_vec.push_back(Disassembler::disassembleInstruction(byte0,byte1));
-            }
-        }
-        std::cout<<"Done.\nOpening Output File...";
-        uint16_t location=0x200;
-        std::ofstream f(splitString(file,{{'/',false},{'\\',false}}).back()+".c8asm");
-        if(!f){
-            std::cout<<"Error.\n Could not open file\n";
-            throw QuitException(1);
-        }
-        std::cout<<"Done.\nWriting to Output File...";
-        for(disassembled_instruction asm_ins:asm_vec){
-            f<<asm_ins.getDisplay(location);
-            location+=2;
-        }
-        std::cout<<"Done.\n";
-    }else if(args.hasOption("debug")){
-        std::cout<<"Initializing Debugger...\n";
-        throw std::runtime_error("Debugging not Implemented yet");
-    }else{
-        std::cout<<"Initializing Chip8 CPU...\n";
-        CPU cpu;
-        std::cout<<"Done.\nInitializing SDL Handler (IO)...";
-        SDLHandler display;
-        std::cout<<"Done.\nLoading ROM...";
-        cpu.loadProgram(file);
-        std::cout<<"Done.\nStarting Emulation.\n";
-        while(1){
-            try{
-                if(display.update(cpu)){
-                    break;
+        if(args.hasOption("disassemble")){
+            printw("Opening Input File...");
+            Program p=FileLoader::load(file,4096-0x200);
+            std::vector<disassembled_instruction> asm_vec;
+            printw("Done.\nStarting Disassembly...");
+            for(unsigned i=0;i<p.length;){
+                uint8_t byte0=p.data[i++];
+                if(i<p.length){
+                    uint8_t byte1=p.data[i++];
+                    asm_vec.push_back(Disassembler::disassembleInstruction(byte0,byte1));
                 }
-                cpu.doCycle();
-            }catch(QuitException &e){
-                throw;
-            }catch(...){
-                SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"Exeption Thrown","An Exeption was Thrown by the Emulator, Aborting execution.",0);
-                throw;
+            }
+            printw("Done.\nOpening Output File...");
+            uint16_t location=0x200;
+            std::ofstream f(splitString(file,{{'/',false},{'\\',false}}).back()+".c8asm");
+            if(!f){
+                printw("Error.\n Could not open file\n");
+                throw QuitException(1);
+            }
+            printw("Done.\nWriting to Output File...");
+            for(disassembled_instruction asm_ins:asm_vec){
+                f<<asm_ins.getDisplay(location);
+                location+=2;
+            }
+            printw("Done.\n");
+        }else if(args.hasOption("debug")){
+            printw("Initializing Debugger...\n");
+            Debugger debugger;
+            debugger.startDebug();
+        }else{
+            printw("Initializing Chip8 CPU...\n");
+            CPU cpu;
+            printw("Done.\nLoading ROM...");
+            cpu.loadProgram(file);
+            printw("Done.\nOpening Display Window...");
+            SDLHandler display;
+            printw("Done.\nStarting Emulation.\n");
+            while(1){
+                try{
+                    if(display.update(cpu)){
+                        break;
+                    }
+                    cpu.doCycle();
+                }catch(QuitException &e){
+                    throw;
+                }catch(...){
+                    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"Exeption Thrown","An Exeption was Thrown by the Emulator, Aborting execution.",0);
+                    throw;
+                }
             }
         }
-    }
     }catch(QuitException &e){
         SDL_Quit();
         endwin();
