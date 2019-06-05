c64d8bcf7d0bbae897d867e0742d78c716add6f7
RicardoLuis0 <ricolvs123@gmail.com>
2019-06-05 16:53:26 -0300

    ramdump,vramdump commands

--- src/Debugger/commands/ramdump.cpp
+++ src/Debugger/commands/ramdump.cpp
@@ -1,5 +1,15 @@
 #include "Debugger.h"
+#include <curses.h>
+#include <fstream>
 
 void Debugger::command_ramdump(std::vector<std::string> args){
-    
+    std::ofstream dump("data.ramdump",std::ios::binary);
+    if(!dump){
+        printw("Could not write to 'data.ramdump'\n");
+        return;
+    }
+    for(uint8_t byte:cpu.RAM){
+        dump<<byte;
+    }
+    printw("RAM written to 'data.ramdump'\n");
 }
