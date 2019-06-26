12fc98e78c1ee9d9f689bd6123a592e80f722acd
RicardoLuis0 <ricolvs123@gmail.com>
2019-06-26 05:03:12 -0300

    fix wrong register

--- src/Debugger/commands/read_registers.cpp
+++ src/Debugger/commands/read_registers.cpp
@@ -6,7 +6,7 @@ void Debugger::command_read_registers(std::vector<std::string> args){
     printw("SP:0x%03X\n",cpu.SP);
     printw("DT:0x%03X\n",cpu.DT);
     printw("ST:0x%03X\n",cpu.ST);
-    printw("I:0x%03X\n",cpu.PC);
+    printw("I:0x%03X\n",cpu.I);
     for(uint8_t i=0;i<16;i++){
         printw("V%X:0x%03X,  KB_%X:%u%s",i,cpu.V[i],i,cpu.KB[i],((i+1)%2?";   ":";\n"));
     }
