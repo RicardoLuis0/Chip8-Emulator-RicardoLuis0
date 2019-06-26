72bc4cddcd95b52e5fc079a249341733699627b2
RicardoLuis0 <ricolvs123@gmail.com>
2019-06-26 05:36:56 -0300

    keypress handling

--- include/SDLHandler.h
+++ include/SDLHandler.h
@@ -10,8 +10,10 @@ class SDLHandler {
         virtual ~SDLHandler();
     protected:
         void drawScreen(CPU &cpu);
+        static int32_t get_key_num(int32_t sym);
         bool pollEvents(CPU &cpu);
         bool handleEvent(CPU &cpu,union SDL_Event * e);
+        void handleKey(CPU &cpu,int32_t sym,bool keyup);
         struct SDL_Renderer * renderer;
         struct SDL_Window * window;
     private:
--- src/SDLHandler.cpp
+++ src/SDLHandler.cpp
@@ -1,6 +1,6 @@
 #include "SDLHandler.h"
 
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 
 #define VRAMPOS(x,y) ((y)*64+(x))
 
@@ -47,10 +47,63 @@ bool SDLHandler::pollEvents(CPU &cpu){
     return doQuit;
 }
 
+int32_t SDLHandler::get_key_num(int32_t sym){
+    switch(sym){
+    case SDLK_1:
+        return 0;
+    case SDLK_2:
+        return 1;
+    case SDLK_3:
+        return 2;
+    case SDLK_4:
+        return 3;
+    case SDLK_q:
+        return 4;
+    case SDLK_w:
+        return 5;
+    case SDLK_e:
+        return 6;
+    case SDLK_r:
+        return 7;
+    case SDLK_a:
+        return 8;
+    case SDLK_s:
+        return 9;
+    case SDLK_d:
+        return 10;
+    case SDLK_f:
+        return 11;
+    case SDLK_z:
+        return 12;
+    case SDLK_x:
+        return 13;
+    case SDLK_c:
+        return 14;
+    case SDLK_v:
+        return 15;
+    default:
+        return -1;
+    }
+}
+
+void SDLHandler::handleKey(CPU &cpu,SDL_Keycode sym,bool keyup){
+    int key=get_key_num(sym);
+    if(key==-1)return;
+    if(keyup){
+        cpu.keyReleased(key);
+    }else{
+        cpu.keyPressed(key);
+    }
+}
+
 bool SDLHandler::handleEvent(CPU &cpu,union SDL_Event * e){
     switch(e->type){
     case SDL_QUIT:
         return true;
+    case SDL_KEYDOWN:
+        handleKey(cpu,e->key.keysym.sym,false);
+    case SDL_KEYUP:
+        handleKey(cpu,e->key.keysym.sym,true);
     default:
         break;
     }
