0261fc4c04904109b0f204fca150496faa0b3b53
RicardoLuis0 <ricolvs123@gmail.com>
2019-06-25 23:52:57 -0300

    fix left shift

--- src/CPU/operations/bitwise_operations.cpp
+++ src/CPU/operations/bitwise_operations.cpp
@@ -14,12 +14,12 @@ void CPU::xor_operation(uint8_t X,uint8_t Y){//move V[X] ^ V[Y] to V[X]
     V[X]^=V[Y];
 }
 
-void CPU::shift_right(uint8_t X,uint8_t Y){//shift V[X] to the right by 1, store least significant bit in V[Y]
+void CPU::shift_right(uint8_t X,uint8_t Y){//shift V[X] to the right by 1, store least significant bit in VF
     VF=V[X]&1;
     V[X]>>=1;
 }
 
-void CPU::shift_left(uint8_t X,uint8_t Y){//shift V[X] to the left by 1, store most significant bit in V[Y]
-    VF=(V[X]<<7)&1;
+void CPU::shift_left(uint8_t X,uint8_t Y){//shift V[X] to the left by 1, store most significant bit in VF
+    VF=(V[X]>>7)&1;
     V[X]<<=1;
 }
